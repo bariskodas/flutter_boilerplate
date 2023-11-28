d4abbbbeef7c07b913fc8c9e8159fdf886b1004b
Barış Kodaş <social.bkodas@gmail.com>
2023-11-28 20:10:38 +0300

    add ✨ : l10n configs and intl packages.

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate/app/l10n/app_localizations.dart';
 import 'package:flutter_flavor/flutter_flavor.dart';
 
 class MyApp extends StatelessWidget {
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'boilerplate',
         debugShowCheckedModeBanner: false,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
         home: Scaffold(
           appBar: AppBar(),
           backgroundColor: Colors.black,
---
+++ lib/app/l10n/app_localizations_tr.dart
@@ -0,0 +1,9 @@
+import 'app_localizations.dart';
+
+/// The translations for Turkish (`tr`).
+class AppLocalizationsTr extends AppLocalizations {
+  AppLocalizationsTr([String locale = 'tr']) : super(locale);
+
+  @override
+  String get hello => 'Merhaba';
+}
