d26b230c0c401f3886f2dcb85e348d6c860fed33
Barış Kodaş <social.bkodas@gmail.com>
2023-11-28 22:54:19 +0300

    update 💫 : network client for pretty_dio_logger package

--- lib/app/data/client/network/dio_client.dart
+++ lib/app/data/client/network/dio_client.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:dio_intercept_to_curl/dio_intercept_to_curl.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 import 'client_interceptors.dart';
 
@@ -23,7 +24,19 @@ class DioClient {
       printOnSuccess: false, //* optional
       convertFormData: true,
     ));
-    
+
+    //? logging
+    dio.interceptors.add(PrettyDioLogger(
+      request: true,
+      requestHeader: false,
+      requestBody: true,
+      responseHeader: false,
+      responseBody: false,
+      error: true,
+      compact: false,
+      maxWidth: 200,
+    ));
+
     return dio;
   }
 }
