68710fadd0402e1ca4c3e1369d577616a6426864
Barış Kodaş <40144559+bariskodas@users.noreply.github.com>
2024-05-14 11:43:35 +0300

    remove 🔥 : flutter_dotenv package

--- lib/flavors/main_prod.dart
+++ lib/flavors/main_prod.dart
@@ -1,9 +1,5 @@
 import 'package:flutter_boilerplate/init.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(fileName: 'assets/env/prod.env');
-  print(dotenv.env['BASE_URL']);
-
   await initApp();
 }
